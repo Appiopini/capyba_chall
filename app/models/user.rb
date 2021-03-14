a6a2ca57f89cd1cf895204fb7ec2a635accb6f79
Appiopini <appiopini@gmail.com>
2021-03-14 00:44:18 -0300

    sign in updated, changed user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
-class User < ApplicationRecord
+class User < ApplicationRecord  
 has_person_name
+attr_accessor :login
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def self.find_for_authentication warden_condition
+    conditions = warden_condition.dup
+    login = considerations.delete(:login)
+    where(conditions).where(
+      ["lower(username) = :value OR lower(email) = :value", {}]).first
+  end
 end
